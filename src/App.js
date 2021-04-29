de74e1edc761b0aa3b94e7649287c2ef7a4abf5b
meru20 <merhawitgubsa@gmail.com>
2021-04-29 13:24:00 -0700

    refactor(App): imported and wrapped the app with global provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from './routers/AppRouter';
+import {GlobalProvider} from './context/GlobalContext';
 
 const App = () => {
   return (
-    <Router>
+    <GlobalProvider>
+      <Router>
       <AppRouter />
     </Router>
+
+    </GlobalProvider>
+    
   );
 };
 
